ebad7b00c58bd7f38f97b892f1ba768ae8b93bbb
ishanparuthi <ishanparuthi>
2025-02-05 07:17:23 +0000

    back

---
+++ Include Paths.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+raw_data = "abfss://sap-test@etlfwstudiodevadls.dfs.core.windows.net"
+processed_data = "abfss://processed@etlfwstudiodevadls.dfs.core.windows.net"
+
+# COMMAND ----------
+
+raw_data
